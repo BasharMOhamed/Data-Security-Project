0baccf30276913bd6cce529eb1e8f294cd8531ca
Basant <basantabdelnaser438@gmail.com>
2024-03-08 02:54:59 +0200

    ceaser,railFaince Done

--- startupcode/securitylibrary/MainAlgorithms/Ceaser.cs
+++ startupcode/securitylibrary/MainAlgorithms/Ceaser.cs
@@ -8,19 +8,81 @@ namespace SecurityLibrary
 {
     public class Ceaser : ICryptographicTechnique<string, int>
     {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+
+            string cipherText = "";
+            plainText = plainText.ToLower();
+            int res;
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+
+                    if (plainText[i] == alphabet[j])
+                    {
+
+                        res = (j + key) % 26;
+                        cipherText += alphabet[res];
+                    }
+
+                }
+
+            }
+            return cipherText.ToUpper();
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            cipherText = cipherText.ToLower();
+            string orgText = "";
+            int res;
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+
+                    if (cipherText[i] == alphabet[j])
+                    {
+
+                        res = (j - key) % 26;
+                        if (res < 0)
+                        {
+                            res *= -1;
+                            res = 26 - res;
+
+                        }
+                        orgText += alphabet[res];
+                    }
+
+                }
+
+            }
+            return orgText.ToLower();
         }
 
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            char firstCharOfPt = plainText[0];
+            int indexOfPT = alphabet.IndexOf(firstCharOfPt);
+            char firstCharOfCt = cipherText[0];
+            int indexOfCt = alphabet.IndexOf(firstCharOfCt);
+            int key = (indexOfCt - indexOfPT) % 26;
+            if (key < 0)
+            {
+                key *= -1;
+                key = 26 - key;
+            }
+
+            return key;
         }
     }
 }
--- startupcode/securitylibrary/MainAlgorithms/RailFence.cs
+++ startupcode/securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,103 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            plainText = plainText.ToLower();
+            int key = 1;
+            string CT = "";
+            while (true)
+            {
+                CT = Encrypt(plainText, key);
+                CT = CT.ToLower();
+                if (CT.Equals(cipherText))
+                {
+                    return key;
+                }
+                key++;
+                if (key == cipherText.Length)
+                {
+                    break;
+                }
+            }
+            return key;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            string plainTxt = "";
+            int count = 0;
+            while (count < ((cipherText.Length / key) + (cipherText.Length % key != 0 ? 1 : 0)))
+            {
+
+                for (int i = count; i < cipherText.Length; i += ((cipherText.Length / key) + (cipherText.Length % key != 0 ? 1 : 0)))
+                {
+                    plainTxt += cipherText[i];
+                }
+                count++;
+            }
+            return plainTxt;
+        }
+        public static int IncrementUntilDivisible(int number, int divisor) // pt key 19   2
+        {
+
+            int counter = 0;
+            int x = number;
+            while (x % divisor != 0)
+            {
+                counter++;
+                x++;
+            }
+            return counter;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            int incSize = 0;
+            int count2 = 0;
+            string cipherTxt = "";
+
+            if (plainText.Length % key != 0)  //19 % 2 
+            {
+                incSize = IncrementUntilDivisible(plainText.Length, key);
+
+            }
+
+            if (incSize == 1)
+            {
+                int count = 0;
+                while (count < key)
+                {
+
+                    for (int i = count; i < plainText.Length; i += key) //0 2 4 
+                    {
+
+                        cipherTxt += plainText[i];
+                    }
+                    count++;
+                }
+                return cipherTxt.ToUpper();
+            }
+            else if (incSize > 1 || incSize == 0)
+            {
+                for (int i = 0; i < incSize; i++)
+                {
+                    plainText += "x";
+                }
+                while (count2 < key)
+                {
+                    for (int i = count2; i < plainText.Length; i += key)
+                    {
+                        cipherTxt += plainText[i];
+                    }
+                    count2++;
+                }
+                return cipherTxt.ToUpper();
+            }
+            return cipherTxt.ToUpper();
         }
     }
 }
