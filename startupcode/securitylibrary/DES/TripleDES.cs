e5362746ee8d260dba60d65e7dc102cf0fbd12ff
BasharMOhamed <bashar.eg6645@gmail.com>
2024-04-12 03:19:31 +0200

    Finishing DES & TripleDES

--- startupcode/securitylibrary/DES/TripleDES.cs
+++ startupcode/securitylibrary/DES/TripleDES.cs
@@ -11,14 +11,15 @@ namespace SecurityLibrary.DES
     /// </summary>
     public class TripleDES : ICryptographicTechnique<string, List<string>>
     {
+        DES algo = new DES();
         public string Decrypt(string cipherText, List<string> key)
         {
-            throw new NotImplementedException();
+            return algo.Decrypt(algo.Encrypt(algo.Decrypt(cipherText, key[0]), key[1]), key[0]);
         }
 
         public string Encrypt(string plainText, List<string> key)
         {
-            throw new NotImplementedException();
+            return algo.Encrypt(algo.Decrypt(algo.Encrypt(plainText, key[0]), key[1]), key[0]);
         }
 
         public List<string> Analyse(string plainText,string cipherText)
